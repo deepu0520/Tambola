45d80333b9f762301928b10266f4e66f954c1630
rakes15 <Rakesh@150890>
2020-02-12 16:52:39 +0530

    cash games edit commit

--- app/src/main/java/com/newitzone/tambola/dialog/CashGamesDialog.kt
+++ app/src/main/java/com/newitzone/tambola/dialog/CashGamesDialog.kt
@@ -1,5 +1,6 @@
 package com.newitzone.tambola.dialog
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,11 +9,17 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
+import com.mod.rdpindia.ui.utils.RecyclerItemClickListenr
 import com.newitzone.tambola.PlayActivity
 import com.newitzone.tambola.R
+import com.newitzone.tambola.adapter.PriceAdapter
+import kotlinx.android.synthetic.main.dialog_cash_games.*
 
 class CashGamesDialog : DialogFragment() {
 
@@ -37,9 +44,26 @@ class CashGamesDialog : DialogFragment() {
         state: Bundle?
     ): View? {
         super.onCreateView(inflater, parent, state)
-        val view =
-            activity!!.layoutInflater.inflate(R.layout.dialog_cash_games, parent, false)
+        val view = activity!!.layoutInflater.inflate(R.layout.dialog_cash_games, parent, false)
+        val context:Context = requireContext()
         ButterKnife.bind(this, view)
+        // Initializing an empty ArrayList to be filled with items
+        val menuList: List<String> = resources.getStringArray(R.array.price_list).asList()
+        val adapter = PriceAdapter(menuList,context)
+        recycler_view.layoutManager = LinearLayoutManager(context)
+        recycler_view.adapter = adapter
+        recycler_view.addOnItemTouchListener(RecyclerItemClickListenr(context, recycler_view, object : RecyclerItemClickListenr.OnItemClickListener {
+
+            override fun onItemClick(view: View, position: Int) {
+                //TODO: Use this
+                val dialog = TicketsDialog()
+                val ft: FragmentTransaction = activity!!.supportFragmentManager.beginTransaction()
+                dialog.show(ft, TicketsDialog.TAG)
+            }
+            override fun onItemLongClick(view: View?, position: Int) {
+                TODO("do nothing")
+            }
+        }))
         return view
     }
 
